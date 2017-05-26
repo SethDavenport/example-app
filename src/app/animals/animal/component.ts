8e8e0f02f64fc678323e7c5f35cbe252f4217019
Seth Davenport <seth>
2017-05-26 12:59:38 -0400

    wip

--- src/app/animals/animal/component.ts
+++ src/app/animals/animal/component.ts
@@ -1,54 +1,58 @@
 import { Component, Input, ChangeDetectionStrategy, OnInit } from '@angular/core';
-import { NgRedux, dispatch, select, ObservableStore } from '@angular-redux/store';
+import {
+  NgRedux,
+  dispatch,
+  select,
+  select$,
+  ObservableStore,
+  Transformer, //Wat did I not export this type?
+  FunctionSelector,
+  Fractal,
+} from '@angular-redux/store';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/observable/combineLatest';
 
 import { IAppState } from '../../store/model';
 import { animalComponentReducer } from './reducers';
 
+export const selectSubTotal = state =>
+  (state.ticketPrice || 0) * (state.numTickets || 0);
+export const defaultToZero: Transformer<number, number> = obs$ =>
+  obs$.map(n => n || 0);
+
 /**
  * Fractal component example.
  */
+@FractalStore({
+  basePath: 'basePath', // Reference to method to be called after ngOnInit
+  localReducer: animalComponentReducer,
+})
 @Component({
   selector: 'zoo-animal',
   templateUrl: './component.html',
   styleUrls: ['./component.css'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AnimalComponent implements OnInit {
+export class AnimalComponent {
   static readonly ADD_TICKET = 'ADD_TICKET';
   static readonly REMOVE_TICKET = 'REMOVE_TICKET';
+
+  basePath = () => [this.animalType, 'items', this.key];
 
   @Input() key: string;
   @Input() animalType: string;
 
-  private subStore: ObservableStore<any>;
+  // Change implementation of dispatch - if this is a fractalStore
+  // class, get use a subStore instance instead of ngRedux.
+  @dispatch() addTicket = () => ({ type: 'ADD_TICKET' });
+  @dispatch() removeTicket = () => ({ type: 'REMOVE_TICKET' });
 
-  name$: Observable<string>;
+  @select(selectSubTotal) subTotal$: Observable<number>;
+  @select() name$: Observable<string>;
+
+  @select$('tickets', defaultToZero)
   numTickets$: Observable<number>;
+
+  @select$('ticketPrice', defaultToZero)
   ticketPrice$: Observable<number>;
-  subTotal$: Observable<number>;
-
-  constructor(private ngRedux: NgRedux<IAppState>) {}
-
-  addTicket = () => this.subStore.dispatch({ type: 'ADD_TICKET' });
-  removeTicket = () => this.subStore.dispatch({ type: 'REMOVE_TICKET' });
-
-  // Can't be done in the constructor because it relies on data from the
-  // inputs.
-  ngOnInit() {
-    this.subStore = this.ngRedux.configureSubStore<any>(
-      [this.animalType, 'items', this.key],
-      animalComponentReducer);
-    this.name$ = this.subStore.select('name');
-    this.numTickets$ = this.subStore.select(['tickets'])
-      .map(n => n || 0);
-    this.ticketPrice$ = this.subStore.select('ticketPrice')
-      .map(n => n || 1);
-
-    this.subTotal$ = Observable.combineLatest(
-      this.numTickets$,
-      this.ticketPrice$,
-      (num, price) => num * price);
-  }
 }
